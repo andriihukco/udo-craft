f3369d05e7a07e0be96ff6be3178d24ad50cb7ef
andriihukco <ahuk1312@gmail.com>
2026-04-10 14:01:09 +0300

    fix: remove 'Додати на полотно' button from generation drawer

--- apps/client/src/app/order/_components/GenerationDrawer.tsx
+++ apps/client/src/app/order/_components/GenerationDrawer.tsx
@@ -373,13 +373,6 @@ export function GenerationDrawer({
                         ) : null}
                       </div>
 
-                      {!loading && previewImage && (
-                        <motion.div initial={{ opacity: 0, y: 10 }} animate={{ opacity: 1, y: 0 }}>
-                          <Button className="w-full" onClick={handleAddToCanvas}>
-                            Додати на полотно
-                          </Button>
-                        </motion.div>
-                      )}
                     </motion.div>
                   )}
 
